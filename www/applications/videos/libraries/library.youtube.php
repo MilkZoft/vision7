8bd49e4577b44a74fb6f2b38f684f25fa9f65d33
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2012-03-26 18:10:30 -0600

    youtube library cambaidno file_get_contents por curl

--- www/applications/videos/libraries/library.youtube.php
+++ www/applications/videos/libraries/library.youtube.php
@@ -16,27 +16,44 @@ class YouTube {
 	
 	public function query($query = NULL) {
 		$this->uri = $query;
+
 		return $this->data();
 	}
 	
 	public function search($search = "", $max = 9, $start = 1) {
-		$this->uri = $this->client . "videos/?vq=" . str_replace(" ", "+", trim($search)) . "&start-index=" . $start . "&max-results=" . $max . "&alt=" . strtolower($this->format);
+		$search = str_replace(" ", "+", trim($search));
+
+		$this->uri = $this->client ."videos/?vq=". $search ."&start-index=". $start ."&max-results=". $max ."&alt=". strtolower($this->format);
+		
+		return $this->data();
+	}
+	
+	public function getLists($username, $max = 20, $start = 1) {
+		$this->uri = $this->client ."users/". trim($username) ."/playlists/?v=2&start-index=". $start ."&max-results=". $max ."&alt=". strtolower($this->format);
+		
+		return $this->data();
+	}
+	
+	public function getByList($ID = NULL, $max = 9, $start = 1) {
+		$this->uri = $this->client ."playlists/". trim($ID) ."/?v=2&start-index=". $start ."&max-results=". $max ."&alt=". strtolower($this->format);
+		
 		return $this->data();
 	}
 	
 	public function getByUser($username = NULL, $max = 9, $start = 1) {
-		$this->uri = $this->client . "users/" . trim($username) . "/uploads/?start-index=" . $start . "&max-results=" . $max . "&alt=" . strtolower($this->format);
+		$this->uri = $this->client ."users/". trim($username) ."/uploads/?start-index=". $start ."&max-results=". $max ."&hl=es%2DMX" . "&alt=" . strtolower($this->format);
+		
 		return $this->data();
 	}
 	
 	public function getByID($ID = NULL) {
-		$this->uri = $this->client . "videos/" . trim($ID) . "?alt=" . strtolower($this->format);
+		$this->uri = $this->client ."videos/". trim($ID) ."?alt=". strtolower($this->format);
+		
 		return $this->data();
 	}
 	
 	public function data() {
-		 try {
-			 
+		 try {	 
             $this->results = $this->getResult();
             
             if(!$this->results or is_null($this->results)) {
@@ -48,14 +65,25 @@ class YouTube {
             return FALSE;
         }
 	}
+		
+	public function validVideo($videoID) {
+		$headers = @get_headers($this->client . "videos/" . $videoID);
+		
+		if(!strpos($headers[0], '200')) {
+			#echo "The YouTube video you entered does not exist";
+			return FALSE;
+		}
+		
+		return TRUE;
+	}
 	
 	private function getResult() {
 		switch(strtolower($this->format)) {
 			case "json":
-				$results =  json_decode(@file_get_contents(str_replace(array("&alt=array", "?alt=array"), array("&alt=json", "?alt=json"), $this->uri), FALSE, stream_context_create(array('http' => array('timeout' => 5, 'method' => $this->typeRequest)))));
+				$results = $this->json_request(str_replace(array("&alt=array", "?alt=array"), array("&alt=json", "?alt=json"), $this->uri));
 				
 				if($results) {
-					if($results->{'openSearch$totalResults'}->{'$t'} > 0) {
+					if($results->feed->{'openSearch$totalResults'}->{'$t'} > 0) {
 						return $results;
 					} else {
 						return FALSE;
@@ -63,7 +91,6 @@ class YouTube {
 				} else {
 					return FALSE;
 				}
-			
 			break;
 				
 			case "xml":
@@ -71,18 +98,21 @@ class YouTube {
 			break;
 			
 			case "array":
-				$results = json_decode(@file_get_contents(str_replace(array("&alt=array", "?alt=array"), array("&alt=json", "?alt=json"), $this->uri), FALSE, stream_context_create(array('http' => array('timeout' => 5, 'method' => $this->typeRequest)))));
+				$results = $this->json_request(str_replace(array("&alt=array", "?alt=array"), array("&alt=json", "?alt=json"), $this->uri));
 				
 				if($results) {
 					if(isset($results->entry) and !isset($results->feed)) {
 						$entry = $results->entry;
 						
 						$data = array(
-							"id"      => $this->id($entry->id->{'$t'}),
-							"title"   => $entry->title->{'$t'},
-							"cut"     => $this->cut($entry->title->{'$t'}),
-							"content" => $entry->content->{'$t'},
-							"views"   => $entry->{'yt$statistics'}->viewCount
+							"id"       => (isset($entry->{'media$group'}->{'yt$videoid'}->{'$t'})) ? $entry->{'media$group'}->{'yt$videoid'}->{'$t'} : $this->id($entry->id->{'$t'}),
+							"title"    => $entry->title->{'$t'},
+							"cut"      => $this->cut($entry->title->{'$t'}),
+							"content"  => (isset($entry->content->{'$t'})) ? $entry->content->{'$t'} : $entry->{'media$group'}->{'media$description'}->{'$t'},
+							"keywords" => explode(",", $entry->{'media$group'}->{'media$keywords'}->{'$t'}),
+							"category" => $entry->category[1]->label,
+							"date"     => strtotime($entry->published->{'$t'}),
+							"views"    => $entry->{'yt$statistics'}->viewCount
 						);
 						
 						return $data;
@@ -90,11 +120,14 @@ class YouTube {
 						if($results->feed->{'openSearch$totalResults'}->{'$t'} > 0) {
 							foreach($results->feed->entry as $entry) {
 								$data["videos"][] = array(
-									"id"      => $this->id($entry->id->{'$t'}),
-									"title"   => $entry->title->{'$t'},
-									"cut"     => $this->cut($entry->title->{'$t'}),
-									"content" => $entry->content->{'$t'},
-									"views"   => $entry->{'yt$statistics'}->viewCount
+									"id" 	   => (isset($entry->{'media$group'}->{'yt$videoid'}->{'$t'})) ? $entry->{'media$group'}->{'yt$videoid'}->{'$t'} : $this->id($entry->id->{'$t'}),
+									"title"    => $entry->title->{'$t'},
+									"cut" 	   => $this->cut($entry->title->{'$t'}),
+									"content"  => (isset($entry->content->{'$t'})) ? $entry->content->{'$t'} : $entry->{'media$group'}->{'media$description'}->{'$t'},
+									"keywords" => explode(",", $entry->{'media$group'}->{'media$keywords'}->{'$t'}),
+									"category" => $entry->category[1]->label,
+									"date"     => strtotime($entry->published->{'$t'}),
+									"views"    => $entry->{'yt$statistics'}->viewCount
 								);
 							}
 							
@@ -113,17 +146,31 @@ class YouTube {
 			break;
 			
 			default:
-				return json_decode(@file_get_contents($this->uri, FALSE, stream_context_create(array('http' => array('timeout' => 5, 'method' => $this->typeRequest)))));
+				return $this->json_request($this->uri);
 			break;
 		}
 	}
 	
+	private function json_request($url) {
+		$curl = curl_init();
+		
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		
+		$response = curl_exec($curl);
+		
+		curl_close($curl);
+		
+		return json_decode($response);
+	}
+	
 	private function id($entry) {
 		$_array = explode("/", $entry);
+
 		return $_array[count($_array) - 1];
 	}
 	
-	private function cut($title, $start = 0, $end = 13, $text = "...") {
+	private function cut($title, $start = 0, $end = 50, $text = "...") {
 		return substr($title, $start, $end) . $text;
 	}
 	
